bd0f84510d82b65f4a896ed0dd7466431998b77b
Jonathan Higger <jonathan.higger@gmail.com>
2020-03-02 02:55:00 +0000

    Automatically backed up by Learn

--- db/migrate/01_create_artists.rb
+++ db/migrate/01_create_artists.rb
@@ -3,4 +3,14 @@ class CreateArtists < ActiveRecord::Migration[4.2]
   end
   def down
   end
+  def changes
+    create_table :artist do |t|
+      t.string :name
+      t.string :genre
+      t.integer :age
+      t.string :hometown
+
+    end
+
+  end
 end
